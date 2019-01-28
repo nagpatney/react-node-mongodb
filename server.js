ec2fe81d542fac33ff677ed834e720f8eb6a2d72
alal5 <alal2@sapient.com>
2019-01-28 22:09:22 +0530

    initial commit v1.0

--- server.js
+++ server.js
@@ -1,13 +1,41 @@
-import http from 'http';
+// import http from 'http';
 
-const server = http.createServer();
+// const server = http.createServer();
 
-server.listen(8080);
+// server.listen(8080);
 
-server.on('request',(req,res) =>{
-    res.write('HelloHttp\n');
-    setTimeout(()=>{
-        res.write(' Stream\n');
-        res.end()
-    },3000)
+// server.on('request',(req,res) =>{
+//     res.write('HelloHttp\n');
+//     setTimeout(()=>{
+//         res.write(' Stream\n');
+//         res.end()
+//     },3000)
+// })
+
+import config from './config';
+import apiRouter from './api';
+// import fs from 'fs';
+
+import express from 'express';
+const server = express();
+
+server.set('view engine','ejs');
+
+server.get('/',(req,res)=>{
+    res.render('index',{
+        content:'Hello from <em>EJS</em>'
+    });
 })
+server.use('/api',apiRouter)
+server.use(express.static('public'));
+
+
+// server.get('/about.html',(req,res) =>{
+//     fs.readFile('./about.html',(err,data) =>{
+//         res.send(data.toString())
+//     })
+// });
+
+server.listen(config.port, ()=>{
+    console.log('express on port',config.port)
+});
